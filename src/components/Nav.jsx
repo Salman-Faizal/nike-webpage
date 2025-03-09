78a8129748aa3809d7346b11035ed6b789c78c0f
Salman-Faizal <salmanfaizal1000@gmail.com>
2025-03-09 09:20:45 +0530

    second commit: added the nav bar on the header section of the page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,37 @@
+import {headerLogo} from '../assets/images';
+import {hamburger} from '../assets/icons';
+import {navLinks} from '../constants/';
+
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header className='absolute padding-x py-8 z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <img
+          src={headerLogo}
+          alt="logo"
+          width={130}
+          height={30}
+        />
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className='font-montserrat leading-normal text-lg text-slate-gray'
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <img src={hamburger}
+        alt="menu-icon"
+        height={25}
+        width={25}
+        className='hidden max-lg:block' />
+      </nav>
+    </header>
   )
 }
 
